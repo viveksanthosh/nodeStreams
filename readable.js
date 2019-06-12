81962e8cb178434990ae8434d08b60a392352634
viveksanthosh <viveksanthosh.in@gmail.com>
2019-06-12 22:40:41 +0530

    added gradually readable stream

--- readable.js
+++ readable.js
@@ -1,23 +1,33 @@
 const { Readable } = require('stream');
 
-let read = new Readable({
-    highWaterMark: 5,
-    read(size){
-        console.log(this)
+const delay = time => new Promise(r => setTimeout(r, time))
+
+let stream = new Readable({
+    highWaterMark: 3,
+    read(size) {
+        let a = this;
+        console.log('here')
     }
 })
 
-let count =65;
+let count = 65;
 let interval = setInterval(()=> {
-    read.push(String.fromCharCode(count++))
+    stream.push(String.fromCharCode(count++))
     if(count >75)
         clearInterval(interval)
     console.log('pushed ', count)
-}, 200);
+}, 2000);
 
 
-setInterval(()=> {
-    let buffer =  read.read(1);
-    if(buffer)
-    console.log('read -> ',buffer.toString('utf8')  )
-    }, 2000);
+const  reader = async () => {
+    let data = stream.read(1);
+    if (!data)
+        return;
+    console.log('data -> ', data.toString('utf-8'))
+    await delay(4000);
+    reader();
+}
+
+delay(4000).then(reader);
+
+delay(100000).then(console.log)
